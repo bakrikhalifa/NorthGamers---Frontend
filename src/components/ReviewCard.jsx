062088013d702fafcb0dc5576323595554def437
Bakri Khalifa <bakrikhalifa@Bakris-MacBook-Air.local>
2023-01-10 14:13:28 +0000

    updated reviews page

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,5 +1,3 @@
-import ThumbsDown from "./ThumbsDown";
-import ThumbsUp from "./ThumbsUp";
 import { Link } from "react-router-dom";
 
 const ReviewCard = ({ reviews }) => {
@@ -15,16 +13,8 @@ const ReviewCard = ({ reviews }) => {
             ></img>
           </Link>
           <h2>{review.title}</h2>
-          <p>Review ID: {review.review_id}</p>
-          <p>Category: {review.category}</p>
           <p>Designer: {review.designer}</p>
           <p>Owner: {review.owner}</p>
-          <p>Review: {review.review_body}</p>
-          <div>
-            <ThumbsUp />
-            <strong className="votes">Votes: {review.votes}</strong>
-            <ThumbsDown />
-          </div>
         </li>
       ))}
     </ul>
